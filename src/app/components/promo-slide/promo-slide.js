db2a0da4ae570b52e46fccf433c3e3b3809f299a
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-05-03 16:33:04 +0300

    add promo slide

---
+++ src/app/components/promo-slide/promo-slide.js
@@ -0,0 +1,62 @@
+import styles from "./promo-slide.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleLeft} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+export default function PromoSlide (){
+
+    const slide1 = 'https://github.com/PopovYehor/ReactApp/blob/Shop/src/assets/slider-1.jpg?raw=true'
+    const slide2 = 'https://github.com/PopovYehor/ReactApp/blob/Shop/src/assets/slider-2.jpg?raw=true'
+    const slide3 = 'https://github.com/PopovYehor/ReactApp/blob/Shop/src/assets/slider-3.jpg?raw=true'
+    const arrSlide = [slide1, slide2, slide3]
+    const arrSliderMainText = ['aluminium club', 'cool sound', 'be beautiful']
+    const arrSliderDescriptionText = ['expirience ray-ban', 'listen sony', 'put on zara']
+    let ints = 0
+    const [sliderImg, setSliderImg] = useState(arrSlide[0])
+    const [slideMainText, setSlideMainText] = useState(arrSliderMainText[0])
+    const [slideDescriptionText, setSlideDescriptionText] = useState(arrSliderDescriptionText[0])
+
+    
+
+    /* const changerSlide = (int,i)=>{
+        setSlideCount(`${slideCount}${int}${i}`)
+        setSliderImg(arrSlide[slideCount])
+        setSlideMainText(arrSliderMainText[slideCount])
+        setSlideDescriptionText(arrSliderDescriptionText[slideCount])
+        console.log(int, i)
+    } */
+
+    const slider = (e)=>{
+        const target = e.target
+        if (target.getAttribute('id') == 'slider-right'){
+            if(ints < arrSlide.length-1){
+                ints++
+                setSliderImg(arrSlide[ints])
+            }else{
+                ints=0
+                setSliderImg(arrSlide[0])
+                console.log('000')
+            }
+        }
+        console.log(ints)
+    }
+
+    return(
+        <>
+        <div className={styles.promo_slider_wrap}>
+            <img className={styles.slide_item_img} src={sliderImg}></img>
+            <div className={styles.promo_slider_container}>
+                <div className={styles.promo_lider_btn} id="slider-left" onClick={(e)=>slider(e)}><FontAwesomeIcon icon={faAngleLeft} /></div>
+                <div className={styles.promo_slider_description}>
+                    <div  className={`${styles.promo_slider_item_name} ${styles.slider_item_text}`}>
+                        <span>{slideMainText}</span>
+                    </div>
+                    <div className={`${styles.promo_slider_item_description} ${styles.slider_item_text}`}>
+                        <span >{slideDescriptionText}</span>
+                    </div>
+                </div>
+                <div className={styles.promo_lider_btn} id="slider-right" onClick={(e)=>slider(e)}><FontAwesomeIcon icon={faAngleRight} /></div>
+            </div>
+        </div>
+        </>
+    )
+}
