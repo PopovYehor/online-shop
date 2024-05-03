780ed52f480bf8d36a0b5eddc7e30564d1af70a4
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-05-03 16:33:45 +0300

    refactor page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,28 @@
 'use client'
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import Header from "./view/header/header";
+import Footer from "./view/footer/footer";
+import PromoSlide from "./components/promo-slide/promo-slide";
 export default function Home() {
 
-  const postsUrl = 'https://popov-test-server.onrender.com/api/posts'
+  /* const postsUrl = 'https://popov-test-server.onrender.com/api/posts' */
 
   const [posts, setPosts] = useState([])
 
-  useEffect(()=>{
+  /* useEffect(()=>{
     axios.get(postsUrl).then((res)=>{
       const {data} = res
       setPosts(data)
     })
-  },[])
+  },[]) */
 
   return (
    <>
     <div>
-      {posts.map((post)=>{
-        return(
-          <p key={post._id}>{post.author}</p>
-        )
-      })}
+      <Header/>
+      <PromoSlide/>
+      <Footer/>
     </div>
    </>
   );
