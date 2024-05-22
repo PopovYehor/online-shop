94b8d753c4337bed7310dc7dcf8ab37d487fffe7
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-05-22 19:45:46 +0300

    ref index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 'use client'
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { fetchCatalog } from "@/reducers/catalogReducer";
+import { SET_CURRENT_JEWELERY } from "@/reducers/jeweleryReducer";
+import { SET_CURRENT_ELECTRONICS } from "@/reducers/electronicsReducer";
+import { SET_CURRENT_MAN_CLOTHING } from "@/reducers/manClothingReducer";
+import { SET_CURRENT_WOMAN_CLOTHING } from "@/reducers/womanClothingReducer";
 import axios from "axios";
 import Header from "@/components/view/header/header";
 import Footer from "@/components/view/footer/footer";
@@ -7,19 +13,42 @@ import PromoSlide from "@/components/promo-slide/promo-slide";
 import CatalogItem from "@/components/catalog-item/catalog-item";
 import SaleSlide from "@/components/sale-slide/sale-slide";
 import MainTitle from "@/components/main-title/main-title";
-
+import { useAppDispatch } from "@/hooks/hooks";
 export default function Home() {
 
   /* const postsUrl = 'https://popov-test-server.onrender.com/api/posts' */
+  const dispatch = useAppDispatch()
+  const catalog = useSelector((state)=>state.catalog)
 
-  const [posts, setPosts] = useState([])
+  useEffect(()=>{
+    if(catalog.status == 'idle'){
+      dispatch(fetchCatalog())
+    }
+  },[])
 
-  /* useEffect(()=>{
-    axios.get(postsUrl).then((res)=>{
-      const {data} = res
-      setPosts(data)
-    })
-  },[]) */
+  useEffect(()=>{
+    if(catalog.status == 'idle'){
+      let manClothing = []
+      let womanClothing = []
+      let jewelery = []
+      let electronics = []
+      catalog.catalog.data.forEach((elem)=>{
+          if (elem.category == "men's clothing"){
+              manClothing = [...manClothing, elem]
+          }else if(elem.category == "women's clothing"){
+              womanClothing = [...womanClothing, elem]
+          }else if(elem.category == "jewelery"){
+              jewelery = [...jewelery, elem]
+          }else(
+              electronics = [...electronics, elem]
+          )
+      })
+      dispatch(SET_CURRENT_MAN_CLOTHING(manClothing))
+      dispatch(SET_CURRENT_WOMAN_CLOTHING(womanClothing))
+      dispatch(SET_CURRENT_ELECTRONICS(electronics))
+      dispatch(SET_CURRENT_JEWELERY(jewelery))
+    }
+  },[catalog])
 
   return (
    <>
@@ -27,10 +56,10 @@ export default function Home() {
       <Header/>
       <PromoSlide/>
       <div className="main-wrap">
-        <MainTitle/>
+        <MainTitle title = {'CATALOG'}/>
         <div className="catalog-wripper">
           <div className="catalog-container">
-            <CatalogItem/>
+            <CatalogItem catalog_item={catalog.catalog}/>
             <SaleSlide/>
           </div>
         </div>
