457c98ea2496a9752d5382b8afeb4251e6c419a1
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-05-25 12:17:50 +0300

    ref buy button components

--- src/components/catalog-item/buy-button/buy-button.js
+++ src/components/catalog-item/buy-button/buy-button.js
@@ -1,46 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import styles from './buy-button.module.scss'
 import DoneIcon from '@mui/icons-material/Done'
 import { LoadingButton } from '@mui/lab'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from '@/hooks/hooks'
-import { SET_PREVIOUS_CART } from '@/reducers/cartReducer'
+import { AddToBasket } from '@/helpers/cartScripts'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Button } from '@mui/material'
+
 export function BuyButton({id}){
     const [transition, setTransition] = useState(false)
     
     const cart = useSelector((state)=>state.cart.cart)
-    const previousCart = useSelector((state)=>state.cart.previous)
+    const catalog = useSelector((state)=>state.catalog.catalog)
     const dispatch = useAppDispatch()
 
-    const getAllBefore = ()=> {
-        let i = cart.indexOf(cart[cart.length-1])
-        let arr = cart
-        arr = i > -1 ? cart.slice(0, i) : []
-        dispatch(SET_PREVIOUS_CART(arr))
+    const setTransitions = (e)=>{
+        setTransition(true)
+        setTimeout(()=>{
+            AddToBasket(e,cart, catalog, dispatch)
+        }, 2000)
     }
-    
-    useEffect(()=>{
-        previousCart.some(element => element.id == id) ? 
-        (setTransition(true))
-        : (setTimeout(()=>{setTransition(true)}, 2000))
-        /* console.log( previousCart.some(element => element.id == id)) */
-        getAllBefore()
-        console.log(cart[cart.length-1].id == id)
-    },[])
 
     return(
-        cart[cart.length-1].id == id && transition == true?
-        <button id={id} className={styles.catalog_item_buy_btn}>
-            <DoneIcon id={id} className={styles.done_icon}/>
-        </button>
-        :
-        transition == false ?
-        <LoadingButton loading variant="outlined" size="small" className={styles.loading_btn}>
-            Loading
-        </LoadingButton>
-        :
-        <button id={id} className={styles.catalog_item_buy_btn}>
-            <DoneIcon id={id} className={styles.done_icon}/>
-        </button>
+        <>
+            {
+                cart.some((element)=>element.id == id) ?
+                    <button id={id} className={styles.catalog_item_buy_btn}>
+                        <DoneIcon id={id} className={styles.done_icon}/>
+                    </button>
+                : transition == true ?
+                <LoadingButton loading variant="outlined" size="small" className={styles.loading_btn}>
+                    Loading
+                </LoadingButton>
+                :
+                <Button variant="contained" size='small'
+                id={id}
+                className={`${styles.catalog_item_buy_btn} ${styles.buyActive}`} 
+                onClick={(e)=>{setTransitions(e)}}>
+                    <ShoppingCartIcon id={id}/>
+                </Button>
+            }
+        </>
     )
 }
--- src/components/catalog-item/catalog-item.js
+++ src/components/catalog-item/catalog-item.js
@@ -1,35 +1,7 @@
 import styles from "./catalog-item.module.scss"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import { useAppDispatch } from "@/hooks/hooks";
-import { useSelector } from "react-redux";
-import { AddToBasket } from "@/helpers/cartScripts";
-import { useState } from "react";
 import {BuyButton} from "./buy-button/buy-button";
 export default function CatalogItem ({catalog_item}){
-
-    const dispatch = useAppDispatch()
-    const catalog = useSelector((state)=>state.catalog.catalog)
-    const cart = useSelector((state)=>state.cart.cart)
-
-    /* useEffect(()=>{
-        cart.some(element => element.id == catalog_item.data.id) ?
-            (setTransition(true), setCartCheck(true))
-            : (setTransition(false), setCartCheck(false))
-    },[catalog_item]) */
-
-    /* const handleCartChange = (event)=>{
-        const targetId = event.target.id
-        let id = ''
-        targetId == '' ? id= event.target.parentNode.id : id = targetId
-        let cartCheck = cart.some(elem => elem.id == id)
-        if (!cartCheck){
-            catalog.data.forEach(elem => {
-                elem.id == id ? dispatch(SET_CURRENT_CART(elem)) : null
-            })
-        }
-    } */
 
 return(
     <>  
@@ -46,15 +18,7 @@ return(
                                 <span className={styles.catalog_item_price}>{elem.price} $</span>
                             </div>
                             <div className={styles.catalog_item_buy} id={elem.id}>
-                                {
-                                cart.some(element => element.id == elem.id) ?
-                                    <BuyButton id={elem.id}/>
-                                : <button id={elem.id}
-                                    className={styles.catalog_item_buy_btn}
-                                    onClick={(e)=>{AddToBasket(e, cart, catalog, dispatch)}}>
-                                    <FontAwesomeIcon id={elem.id} icon={ faBasketShopping}/>
-                                </button>
-                                }
+                                <BuyButton id={elem.id}/>   
                             </div>
                         </div>
                     </div>
