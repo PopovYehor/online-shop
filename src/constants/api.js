8dcc0c2bfe03e43cda77b45ae88d70222732b47d
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-06-08 15:33:33 +0300

    add constants

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:5000/api/catalog'
+/* export const API_URL = 'http://localhost:5000/api/catalog' */
+export const API_URL = 'https://fakestoreapi.com/products'
 export const ORDER_URL = 'http://localhost:5000/order'
