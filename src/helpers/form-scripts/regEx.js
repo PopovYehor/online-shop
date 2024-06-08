5e4ce1d067b5d93233f02b4436df71d6199e3b3c
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-06-08 15:35:20 +0300

    Update regEx.js

--- src/helpers/form-scripts/regEx.js
+++ src/helpers/form-scripts/regEx.js
@@ -1,4 +1,4 @@
-export const regExName = /([a-zA-Z]+\s)+[a-zA-Z]+/
+export const regExName = /([a-zA-Zа-яА-я]+\s)+[a-zA-Zа-яА-Я]+/
 export const regExEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 export const regExPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
 export const regExAddres = /[A-Za-z0-9'\.\-\s\,]/
