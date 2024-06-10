82d1ab4ada3e84673112749c95b8a32356f4f5c9
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-06-10 12:35:20 +0300

    update done page

--- src/pages/done/index.js
+++ src/pages/done/index.js
@@ -2,13 +2,28 @@ import BottomNav from "@/components/bottom-nav/bottom-nav";
 import DoneItem from "@/components/done-item/done-item";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 export default function Done(){
+    const router = useRouter()
+    const order = useSelector((state)=>state.order.order)
+    const [pageCheck, setPageCheck] = useState(false)
+
+    useEffect(()=>{
+        order.name == null ? (setPageCheck(false), router.push('/')) : setPageCheck(true)
+    },[])
+
     return(
         <>
-        <Header/>
-        <DoneItem/>
-        <Footer/>
-        <BottomNav/>
+        {pageCheck &&
+            <>
+            <Header/>
+            <DoneItem/>
+            <Footer/>
+            <BottomNav/>
+            </>
+        }
         </>
     )
 }
